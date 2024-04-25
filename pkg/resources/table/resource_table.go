81b90bb226d9d8b80d191d7724a01e7b5aa67e29
Jesse Grodman <jesse@triplewhale.com>
2024-04-25 14:55:48 +0300

    fix - cluster

--- pkg/resources/table/resource_table.go
+++ pkg/resources/table/resource_table.go
@@ -182,8 +182,8 @@ func resourceTableCreate(ctx context.Context, d *schema.ResourceData, meta any) 
 	tableResource.OrderBy = common.MapArrayInterfaceToArrayOfStrings(d.Get("order_by").([]interface{}))
 	tableResource.SetPartitionBy(d.Get("partition_by").([]interface{}))
 
-	if tableResource.Cluster != "" {
-		tableResource.Cluster = client.DefaultCluster
+	if tableResource.Cluster == "" {
+		tableResource.Cluster = client.DefaultCluster // bug
 	}
 
 	tableResource.Validate(diags)
