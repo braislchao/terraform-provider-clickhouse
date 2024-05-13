eaa62ebd271ec3317897c16040595ea048a0a8d8
Cheskel Twersky <twerskycheskel@gmail.com>
2024-05-13 22:27:55 +0300

    init format

--- pkg/resources/table/service.go
+++ pkg/resources/table/service.go
@@ -113,7 +113,7 @@ func (ts *CHTableService) getTableColumns(ctx context.Context, database string, 
 }
 
 func (ts *CHTableService) CreateTable(ctx context.Context, tableResource TableResource) error {
-	query := buildCreateOnClusterSentence(tableResource)
+	query := formatSQL(buildCreateOnClusterSentence(tableResource))
 	err := (*ts.CHConnection).Exec(ctx, query)
 	if err != nil {
 		return fmt.Errorf("creating Clickhouse table: %v", err)
