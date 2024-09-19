515e86519d9ddbdcec3ce07dba9e43630af89423
benjaminawd-flowdesk <benjamin.dornel@flowdesk.co>
2024-09-19 18:51:22 +0800

    fix: do not remove TTL if table does not have it

--- pkg/sdk/tables_ttl.go
+++ pkg/sdk/tables_ttl.go
@@ -3,16 +3,24 @@ package sdk
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/FlowdeskMarkets/terraform-provider-clickhouse/pkg/models"
 )
 
 func UpdateTTL(ctx context.Context, c *Client, table models.TableResource, clusterStatement string, newTTL map[string]interface{}) error {
-	removeTTLQuery := fmt.Sprintf("ALTER TABLE %s.%s %s REMOVE TTL",
-		table.Database, table.Name, clusterStatement)
-	err := executeQuery(ctx, c, removeTTLQuery)
+	hasTTL, err := tableHasTTL(ctx, c, table)
 	if err != nil {
 		return err
+	}
+
+	if hasTTL {
+		removeTTLQuery := fmt.Sprintf("ALTER TABLE %s.%s %s REMOVE TTL",
+			table.Database, table.Name, clusterStatement)
+		err = executeQuery(ctx, c, removeTTLQuery)
+		if err != nil {
+			return err
+		}
 	}
 
 	for k, v := range newTTL {
@@ -25,4 +33,20 @@ func UpdateTTL(ctx context.Context, c *Client, table models.TableResource, clust
 	}
 
 	return nil
+}
+
+func tableHasTTL(ctx context.Context, c *Client, table models.TableResource) (bool, error) {
+	query := fmt.Sprintf(
+		"SELECT create_table_query FROM system.tables where database = '%s' and table = '%s'", table.Database, table.Name,
+	)
+	row := c.Conn.QueryRow(ctx, query)
+	var (
+		create_table_query string
+	)
+
+	if err := row.Scan(&create_table_query); err != nil {
+		return false, err
+	}
+
+	return strings.Contains(create_table_query, "TTL"), nil
 }
