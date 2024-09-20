3060e2d1b3084dfeeb1bddb0a74b8fd8b9b311bb
benjaminawd-flowdesk <benjamin.dornel@flowdesk.co>
2024-09-20 10:40:46 +0800

    fix: simplify TTL, reapply all if any changes else remove

--- pkg/sdk/tables_ttl.go
+++ pkg/sdk/tables_ttl.go
@@ -9,55 +9,30 @@ import (
 )
 
 func UpdateTTL(ctx context.Context, c *Client, table models.TableResource, clusterStatement string, newTTL map[string]interface{}) error {
-	hasTTL, err := tableHasTTL(ctx, c, table)
-	if err != nil {
-		return err
+	var ttlExprs []string
+	for k, v := range newTTL {
+		ttlExpr := fmt.Sprintf("%s %s", k, v)
+		ttlExprs = append(ttlExprs, ttlExpr)
 	}
 
-	// since Clickhouse does not support altering individual
-	// TTL statements, they need to be removed and then reapplied
-	if hasTTL {
-		removeTTLQuery := fmt.Sprintf("ALTER TABLE %s.%s %s REMOVE TTL",
-			table.Database, table.Name, clusterStatement)
-		err = executeQuery(ctx, c, removeTTLQuery)
+	ttlExprsStatement := strings.Join(ttlExprs, ", ")
+	if ttlExprsStatement != "" {
+		modifyTTLQuery := fmt.Sprintf("ALTER TABLE %s.%s %s MODIFY TTL %s",
+			table.Database, table.Name, clusterStatement, ttlExprsStatement)
+		err := executeQuery(ctx, c, modifyTTLQuery)
 		if err != nil {
 			return err
 		}
 	}
 
-	// Re-apply TTL if it was removed
-	if !hasTTL {
-		var ttlExprs []string
-		for k, v := range newTTL {
-			ttlExpr := fmt.Sprintf("%s %s", k, v)
-			ttlExprs = append(ttlExprs, ttlExpr)
-		}
-
-		ttlExprsStatement := strings.Join(ttlExprs, ", ")
-		if ttlExprsStatement != "" {
-			modifyTTLQuery := fmt.Sprintf("ALTER TABLE %s.%s %s MODIFY TTL %s",
-				table.Database, table.Name, clusterStatement, ttlExprsStatement)
-			err = executeQuery(ctx, c, modifyTTLQuery)
-			if err != nil {
-				return err
-			}
+	if ttlExprsStatement == "" {
+		removeTTLQuery := fmt.Sprintf("ALTER TABLE %s.%s %s (REMOVE TTL)",
+			table.Database, table.Name, clusterStatement)
+		err := executeQuery(ctx, c, removeTTLQuery)
+		if err != nil {
+			return err
 		}
 	}
 
 	return nil
-}
-
-func tableHasTTL(ctx context.Context, c *Client, table models.TableResource) (bool, error) {
-	query := fmt.Sprintf(
-		"SELECT create_table_query FROM system.tables where database = '%s' and table = '%s'",
-		table.Database, table.Name,
-	)
-	var createTableQuery string
-	row := c.Conn.QueryRow(ctx, query)
-
-	if err := row.Scan(&createTableQuery); err != nil {
-		return false, err
-	}
-
-	return strings.Contains(createTableQuery, "TTL"), nil
 }
